28011d2118472a59575e108e9e2bcad61b725787
unknown <naerys.404@gmail.com>
2023-07-17 18:36:29 +0200

    admin: liste des commandes a traiter + modification de l'adresse et du statut de la commande

--- src/Controller/AdminOrdersController.php
+++ src/Controller/AdminOrdersController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminOrdersController extends AbstractController
 {
