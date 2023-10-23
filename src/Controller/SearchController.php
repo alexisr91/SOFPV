b4abc25dc4b03b0515e5ae4bde5edc211748bdbf
unknown <naerys.404@gmail.com>
2023-10-23 23:09:06 +0200

    search bar clean update

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -27,6 +27,7 @@ class SearchController extends AbstractController
         $users = $userRepo->findByPseudo($q, $active);
         $category = $categoryRepo->findByName($q, $active);
 
+
         return $this->render('search/index.html.twig', [
             'articles' => $articles,
             'users' => $users,
